8f995839abff9cb3bf979f17342aed14b14d5a4a
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2012-01-28 03:38:46 +0000

    core:changed error message when string in static method valueOf(String) contains invalid sigla

--- trunk/core/src/main/java/cz/nkp/urnnbn/core/Sigla.java
+++ trunk/core/src/main/java/cz/nkp/urnnbn/core/Sigla.java
@@ -13,13 +13,13 @@ import java.util.regex.Pattern;
  */
 public class Sigla {
 
-    private static Pattern urnNbnPattern = Pattern.compile("[a-zA-z]{3}[0-9]{3}");
+    private static Pattern SIGLA_PATTERN = Pattern.compile("[a-zA-z]{3}[0-9]{3}");
     private final String value;
 
     public static Sigla valueOf(String string) {
-        Matcher matcher = urnNbnPattern.matcher(string);
+        Matcher matcher = SIGLA_PATTERN.matcher(string);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException("sigla '" + string + "' is invalid");
+            throw new IllegalArgumentException("'" + string + "' doesn't match " + SIGLA_PATTERN);
         }
         return new Sigla(string);
     }
