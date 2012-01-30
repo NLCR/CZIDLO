fe152f56386f5462b235ae825ee2a204e24aa297
Martin.Rehanek <Martin.Rehanek@gmail.com>
2012-01-30 01:15:05 +0000

    xml: changed and finished implementation of adding digital instances

--- xml/src/main/java/cz/nkp/urnnbn/xml/builders/DigitalInstanceBuilder.java
+++ xml/src/main/java/cz/nkp/urnnbn/xml/builders/DigitalInstanceBuilder.java
@@ -15,12 +15,21 @@ public class DigitalInstanceBuilder extends XmlBuilder {
 
     private final DigitalInstance instance;
     private final DigitalLibraryBuilder digLibBuilder;
+    private final Long digLibId;
     private final DigitalRepresentationBuilder digRepBuilder;
 
     public DigitalInstanceBuilder(DigitalInstance instance, DigitalLibraryBuilder digLibBuilder, DigitalRepresentationBuilder digRepBuilder) {
         this.instance = instance;
         this.digLibBuilder = digLibBuilder;
+        this.digLibId = null;
         this.digRepBuilder = digRepBuilder;
+    }
+
+    public DigitalInstanceBuilder(DigitalInstance instance, Long digLibId) {
+        this.instance = instance;
+        this.digLibBuilder = null;
+        this.digLibId = digLibId;
+        this.digRepBuilder = null;
     }
 
     @Override
@@ -31,6 +40,7 @@ public class DigitalInstanceBuilder extends XmlBuilder {
         appendElementWithContentIfNotNull(root, instance.getPublished(), "published");
         appendBuilderResultfNotNull(root, digLibBuilder);
         appendBuilderResultfNotNull(root, digRepBuilder);
+        appendElementWithContentIfNotNull(root, digLibId, "digitalLibraryId");
         return root;
     }
 }
