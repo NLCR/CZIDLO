8a86037f7d08baa3002e3ca304c78d646551c7bc
Martin.Rehanek <Martin.Rehanek@gmail.com>
2012-02-08 02:34:09 +0000

    core: equals and hashcode overwritten in RegistrarCode

--- core/src/main/java/cz/nkp/urnnbn/core/RegistrarCode.java
+++ core/src/main/java/cz/nkp/urnnbn/core/RegistrarCode.java
@@ -32,4 +32,26 @@ public class RegistrarCode {
     public String toString() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RegistrarCode other = (RegistrarCode) obj;
+        if ((this.value == null) ? (other.value != null) : !this.value.equals(other.value)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (this.value != null ? this.value.hashCode() : 0);
+        return hash;
+    }
 }
