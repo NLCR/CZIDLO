b56c20f1f499da4073c93dc6a39335bda7f9de47
Martin.Rehanek <Martin.Rehanek@gmail.com>
2012-02-20 08:52:01 +0000

    persistence: unifying terminology

--- persistence/src/main/java/cz/nkp/urnnbn/core/persistence/DigDocIdentifierDAO.java
+++ persistence/src/main/java/cz/nkp/urnnbn/core/persistence/DigDocIdentifierDAO.java
@@ -34,8 +34,8 @@ public interface DigDocIdentifierDAO {
      * @param digDocDbId
      * @param idType
      * @throws DatabaseException
-     * @throws RecordNotFoundException if digital representation with id digRepDbId doesn't exist 
-     * or the identifier for digital representation with id digRepDbId and type idType doesn't exist
+     * @throws RecordNotFoundException if digital document with id digRepDbId doesn't exist 
+     * or the identifier for digital document with id digRepDbId and type idType doesn't exist
      */
     public void deleteDigDocIdentifier(long digDocDbId, DigDocIdType idType) throws DatabaseException, RecordNotFoundException;
 
@@ -43,7 +43,7 @@ public interface DigDocIdentifierDAO {
      * 
      * @param digDocDbId
      * @throws DatabaseException
-     * @throws RecordNotFoundException if digital representation with id digRepDbId doesn't exist
+     * @throws RecordNotFoundException if digital document with id digRepDbId doesn't exist
      */
     public void deleteAllIdentifiersOfDigDoc(long digDocDbId) throws DatabaseException, RecordNotFoundException;
 }
