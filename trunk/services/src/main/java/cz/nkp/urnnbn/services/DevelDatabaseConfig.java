3ae9a40c4e1a7382e44ce4298c576dd4389dd04e
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2012-03-09 04:10:10 +0000

    services: initializing database connection from here instead of module 'api', singleton with factory methods for services

---
+++ trunk/services/src/main/java/cz/nkp/urnnbn/services/DevelDatabaseConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.nkp.urnnbn.services;
+
+/**
+ *
+ * @author Martin Řehánek
+ */
+public class DevelDatabaseConfig {
+	public static String DRIVER = "org.postgresql.Driver";
+	public static String HOST = "localhost";
+	public static String DATABASE = "resolver";
+	public static int PORT = 5432;
+	public static String LOGIN = "testuser";
+	public static String PASSWORD = "testpass";
+}
