360f12a572acf19d3adccbd87cface5506652600
Martin.Rehanek <Martin.Rehanek@gmail.com>
2012-03-13 06:50:07 +0000

    web-common

--- web-common/src/main/java/cz/nkp/urnnbn/webcommon/config/PropertyKeys.java
+++ web-common/src/main/java/cz/nkp/urnnbn/webcommon/config/PropertyKeys.java
@@ -9,11 +9,7 @@ package cz.nkp.urnnbn.webcommon.config;
  * @author Martin Řehánek
  */
 public class PropertyKeys {
-    //for both modules
-    public static final String SERVER_READ_ONLY = "server.readOnly";
-    public static final String DEVEL = "develMode";
-    //for module api
-    public static final String URN_RESERVATION_DEFAULT_SIZE = "api.reservation.defaultSize";
-    public static final String URN_RESERVATION_MAX_SIZE = "api.reservation.maxSize";
-    public static final String MAX_RESERVED_SIZE_TO_PRINT = "api.getReseravations.maxReservedToPrint";
+
+    public static final String SERVER_READ_ONLY = "resolver.readOnly";
+    public static final String DEVEL = "resolver.develMode";
 }
