de390c903d98badab71535adfcdb62655405996e
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2012-03-13 06:48:11 +0000

    rest:refactoring

---
+++ trunk/rest/src/main/java/cz/nkp/urnnbn/rest/config/PropertyKeys.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.nkp.urnnbn.rest.config;
+
+/**
+ *
+ * @author Martin Řehánek
+ */
+public class PropertyKeys extends cz.nkp.urnnbn.webcommon.config.PropertyKeys {
+
+    public static final String URN_RESERVATION_DEFAULT_SIZE = "api.reservation.defaultSize";
+    public static final String URN_RESERVATION_MAX_SIZE = "api.reservation.maxSize";
+    public static final String MAX_RESERVED_SIZE_TO_PRINT = "api.getReseravations.maxReservedToPrint";
+}
