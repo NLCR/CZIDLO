2272c953dcc661e0fa30e03c2ce2dd2b21383613
honza.rychtar@gmail.com <honza.rychtar@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2012-03-29 09:52:46 +0000

    LegacyRecordIport:"addDigitalInstance" document builder.

--- trunk/legacyRecordsImport/src/main/java/cz/nkp/urnnbn/legacyrecordsimport/RecordImportBuilder.java
+++ trunk/legacyRecordsImport/src/main/java/cz/nkp/urnnbn/legacyrecordsimport/RecordImportBuilder.java
@@ -48,14 +48,14 @@ public class RecordImportBuilder {
     void buildFiles() {
         try {
             Statement st = con.createStatement();
-            String statement = "select  r.UPDATE_TIMESTAMP as DD_updated,r.CISLO_RDCZ, r.URNNBN,"
+            String statement = "select  r.DR_ID, r.UPDATE_TIMESTAMP as DD_updated,r.CISLO_RDCZ, r.URNNBN,"
                     + "r.PRIDELENO_DNE as URN_prideleno, r.FORMAT, r.DOSTUPNOST, r.SIGLA,"
                     + "r.CISLO_ZAKAZKY, r.FINANCOVANO, e.UPDATE_TIMESTAMP as IE_updated,"
                     + "e.CCNB, e.ISSN, e.DRUH_DOKUMENTU, e.NAZEV, e.AUTOR, e.ROK_VYDANI, "
-                    + "e.ROCNIK_PERIODIKA, e.MISTO_VYDANI"
+                    + "e.ROCNIK_PERIODIKA, e.MISTO_VYDANI, z.URL"
                     + " from "
                     + "    (select "
-                    + "    i.SIGLA,"
+                    + "    i.SIGLA, r1.DR_ID,"
                     + "    r1.UPDATE_TIMESTAMP,r1.CISLO_RDCZ, r1.URNNBN,"
                     + "    r1.PRIDELENO_DNE, r1.FORMAT, r1.DOSTUPNOST, "
                     + "    r1.CISLO_ZAKAZKY, r1.FINANCOVANO, r1.INTELEKTUALNI_ENTITA"
@@ -63,6 +63,7 @@ public class RecordImportBuilder {
                     + "    URNNBN.DIGITALNI_REPREZENTACE  r1 left outer join URNNBN.INSTITUCE  i"
                     + "    on r1.INSTITUCE=i.INSTITUCE_ID) r left outer join URNNBN.INTELEKTUALNI_ENTITA e"
                     + "                                      on r.INTELEKTUALNI_ENTITA=e.IE_ID "
+                    + " left join URNNBN.ZVEREJNENO z on r.DR_ID =z.DIGITALNI_REPREZENTACE"
                     + "                                      where e.DRUH_DOKUMENTU='BK'";
             ResultSet resultSet = st.executeQuery(statement);
             buildImports(resultSet);
@@ -76,11 +77,14 @@ public class RecordImportBuilder {
         while (resultSet.next()) {
             try {
                 buildImport(resultSet);
-            } catch (Throwable ex) {
-                Logger.getLogger(RecordImportBuilder.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SiglaNotFoundException ex) {
+                System.out.println("EXC:SIGLA " + ex.getMessage());
+            } catch (IOException ex) {
+                System.out.println("EXC:IO " + ex.getMessage());
             }
 
-            if (++counter > 300) {
+
+            if (++counter > 800) {
                 break;
             }
 
@@ -104,9 +108,11 @@ public class RecordImportBuilder {
         return child;
     }
 
-    private void buildImport(ResultSet resultSet) throws SQLException, Exception {
-        String urnNbn = updateUrn(enhanceString(resultSet.getString("URNNBN")));
-        String registrarCode = updateRegistrarCode(enhanceString(resultSet.getString("SIGLA")));
+    private void buildImport(ResultSet resultSet) throws SQLException, SiglaNotFoundException, IOException {
+        //String urnNbn = updateUrn(enhanceString(resultSet.getString("URNNBN")));
+        //String registrarCode = updateRegistrarCode(enhanceString(resultSet.getString("SIGLA")));
+        String urnNbn = enhanceString(resultSet.getString("URNNBN"));
+        String registrarCode = enhanceString(resultSet.getString("SIGLA"));
         checkIfFits(urnNbn, registrarCode);
         Element importEl = new Element("r:import", RESOLVER);
         appendEntityElement(importEl, resultSet);
@@ -114,13 +120,35 @@ public class RecordImportBuilder {
 
         Document importDocument = new Document(importEl);
         validateDocument(importDocument, urnNbn);
-        saveImportDocToFile(importDocument, urnNbn);
+        saveImportDocToFile(importDocument, urnNbn,registrarCode);
         //k pozdejsi aktualizaci databaze mimo importy
         saveDateStamps(resultSet, urnNbn);
 
         //patri k ie a stejne tam nic moc neni, tak asi zahodit
         String accessibility = enhanceString(resultSet.getString("DOSTUPNOST"));
+        String url = enhanceString(resultSet.getString("URL"));
+        String format = enhanceString(resultSet.getString("FORMAT"));
+        buildDigitalInstanceDocument(url, registrarCode, format, accessibility, urnNbn);
     }
+    
+    private void buildDigitalInstanceDocument(String url, String registrarCode, String format, 
+            String accessibility, String urnNbn) throws SiglaNotFoundException, IOException {
+        int digitalLibraryId = SiglaLibraryIdMapping.getLibraryId(registrarCode);
+
+        Element rootEl = new Element("r:digitalInstance", RESOLVER);
+        
+        appendElementWithContentIfNotNull(rootEl, url, "url");
+        appendElementWithContentIfNotNull(rootEl, digitalLibraryId, "digitalLibraryId");
+        appendElementWithContentIfNotNull(rootEl, format, "format");
+        appendElementWithContentIfNotNull(rootEl, accessibility, "accessibility");       
+        
+
+        Document instanceDocument = new Document(rootEl);
+        
+        String path = resultDir.getAbsolutePath() + File.separator + registrarCode + File.separator + urnNbn + "-add.xml";
+        saveDocumentToFile(instanceDocument, path);
+    }        
+    
 
     private String updateUrn(String original) {
         if (original.toLowerCase().startsWith("urn:nbn:cz:aba001")) {
@@ -140,9 +168,9 @@ public class RecordImportBuilder {
     
     
 
-    private void checkIfFits(String urnNbn, String registrarCode) throws Exception {      
+    private void checkIfFits(String urnNbn, String registrarCode) throws SiglaNotFoundException {      
         if (!urnNbn.toLowerCase().substring(URNNBN_PREFIX.length()).startsWith(registrarCode.toLowerCase())) {
-            throw new Exception(urnNbn + " dowsn't fit " + registrarCode);
+            throw new SiglaNotFoundException(urnNbn + " dowsn't fit " + registrarCode);
         }
     }
 
@@ -258,12 +286,14 @@ public class RecordImportBuilder {
         return new Document(datastamps);
     }
 
-    private void saveImportDocToFile(Document document, String urnNbn) throws IOException {
-        String path = resultDir.getAbsolutePath() + File.separator + urnNbn + ".xml";
+    private void saveImportDocToFile(Document document, String urnNbn, String registrarCode) throws IOException {
+        String path = resultDir.getAbsolutePath() + File.separator + registrarCode + File.separator + urnNbn + ".xml";
         saveDocumentToFile(document, path);
     }
     
     private void saveDocumentToFile(Document document, String path) throws IOException {
+        File f = new File(path);
+        f.getParentFile().mkdirs();
         FileOutputStream out = new FileOutputStream(path);
         Serializer ser = new Serializer(out, "UTF-8");
         ser.setIndent(2);
