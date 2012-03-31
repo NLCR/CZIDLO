faa98ac1462a6aa71335ff1363eccde2fbe23dad
Martin.Rehanek <Martin.Rehanek@gmail.com>
2012-03-31 10:04:18 +0000

    xml: unmarshaller and xsd for digitalInstance import

---
+++ xml/src/main/java/cz/nkp/urnnbn/xml/unmarshallers/DigitalInstanceUnmarshaller.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.nkp.urnnbn.xml.unmarshallers;
+
+import cz.nkp.urnnbn.core.dto.DigitalInstance;
+import nu.xom.Document;
+import nu.xom.Element;
+
+/**
+ *
+ * @author Martin Řehánek
+ */
+public class DigitalInstanceUnmarshaller extends Unmarshaller {
+
+    private final Document doc;
+
+    public DigitalInstanceUnmarshaller(Document doc) {
+        this.doc = doc;
+    }
+
+    public DigitalInstance getDigitalInstance() {
+        DigitalInstance result = new DigitalInstance();
+        Element root = doc.getRootElement();
+        result.setUrl(elementContentOrNull("url", root));
+        result.setLibraryId(Integer.valueOf(elementContentOrNull("digitalLibraryId", root)));
+        result.setFormat(elementContentOrNull("format", root));
+        result.setAccessibility(elementContentOrNull("accessibility", root));
+        return result;
+    }
+}
