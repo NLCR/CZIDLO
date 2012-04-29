9e6700dd3866def415d2acd2a6731c2863dc62cc
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2012-04-29 14:40:36 +0000

    web: digital library details - url is actually url, not just label

--- web/src/cz/nkp/urnnbn/client/institutions/DigitalLibraryDetailsDialogBox.java
+++ web/src/cz/nkp/urnnbn/client/institutions/DigitalLibraryDetailsDialogBox.java
@@ -4,6 +4,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -28,23 +29,26 @@ public class DigitalLibraryDetailsDialogBox extends AbstractDialogBox {
 	private Panel contentPanel() {
 		VerticalPanel panel = new VerticalPanel();
 		Grid grid = new Grid(determineRows(), 2);
-		grid.setWidget(0, 0, new Label(constants.title() + ':'));
-		grid.setWidget(0, 1, new Label(library.getName()));
-		grid.setWidget(1, 0, new Label(constants.id() + ':'));
-		grid.setWidget(1, 1, new Label(library.getId().toString()));
-		grid.setWidget(2, 0, new Label(constants.description() + ':'));
-		grid.setWidget(2, 1, new Label(library.getDescription()));
-		grid.setWidget(3, 0, new Label(constants.url() + ':'));
-		grid.setWidget(3, 1, new Label(library.getUrl()));
-		grid.setWidget(4, 0, new Label(constants.contains() + ':'));
+		int index = 0;
+		grid.setWidget(index, 0, new Label(constants.title() + ':'));
+		grid.setWidget(index++, 1, new Label(library.getName()));
+		grid.setWidget(index, 0, new Label(constants.id() + ':'));
+		grid.setWidget(index++, 1, new Label(library.getId().toString()));
+		grid.setWidget(index, 0, new Label(constants.description() + ':'));
+		grid.setWidget(index++, 1, new Label(library.getDescription()));
+		if (library.getUrl() != null) {
+			grid.setWidget(index, 0, new Label(constants.url() + ':'));
+			grid.setWidget(index++, 1, new HTML(buildUrlOrNull(library.getUrl())));
+		}
+		grid.setWidget(index, 0, new Label(constants.contains() + ':'));
 		// TODO: dodat opravdový počet dokumentů
-		grid.setWidget(4, 1, new Label("0 dokumentů"));
+		grid.setWidget(index++, 1, new Label("0 dokumentů"));
 		if (library.getCreated() != null) {
-			grid.setWidget(5, 0, new Label(constants.created() + ':'));
-			grid.setWidget(5, 1, new Label(library.getCreated()));
+			grid.setWidget(index, 0, new Label(constants.created() + ':'));
+			grid.setWidget(index++, 1, new Label(library.getCreated()));
 			if (library.getModified() != null && !library.getModified().equals(library.getCreated())) {
-				grid.setWidget(6, 0, new Label(constants.modified() + ':'));
-				grid.setWidget(6, 1, new Label(library.getModified()));
+				grid.setWidget(index, 0, new Label(constants.modified() + ':'));
+				grid.setWidget(index++, 1, new Label(library.getModified()));
 			}
 		}
 		panel.add(grid);
@@ -52,8 +56,19 @@ public class DigitalLibraryDetailsDialogBox extends AbstractDialogBox {
 		return panel;
 	}
 
+	private String buildUrlOrNull(String target) {
+		if (target != null) {
+			return "<a href=\"" + target + "\">" + target + "</a>";
+		} else {
+			return null;
+		}
+	}
+
 	private int determineRows() {
-		int result = 5;
+		int result = 4;
+		if (library.getUrl() != null) {
+			result++;
+		}
 		if (library.getCreated() != null) {
 			result++;
 			if (library.getModified() != null && !library.getModified().equals(library.getCreated())) {
