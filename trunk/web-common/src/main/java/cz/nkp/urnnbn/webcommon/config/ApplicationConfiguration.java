033500608bc473fb158825e6e9da2416d82ac17d
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2012-08-26 19:48:59 +0000

    web-common: loading configuration updated

--- trunk/web-common/src/main/java/cz/nkp/urnnbn/webcommon/config/ApplicationConfiguration.java
+++ trunk/web-common/src/main/java/cz/nkp/urnnbn/webcommon/config/ApplicationConfiguration.java
@@ -4,6 +4,7 @@
  */
 package cz.nkp.urnnbn.webcommon.config;
 
+import cz.nkp.urnnbn.core.CountryCode;
 import cz.nkp.urnnbn.services.Services;
 import cz.nkp.urnnbn.utils.PropertyLoader;
 import java.io.IOException;
@@ -14,30 +15,50 @@ import java.util.logging.Logger;
  * @author Martin Řehánek
  */
 public class ApplicationConfiguration {
+
     private static final Logger appLogger = Logger.getLogger(ApplicationConfiguration.class.getName());
     private static ApplicationConfiguration instance = null;
     private Boolean serverReadOnly;
     private Boolean develMode;
-    
-   static public ApplicationConfiguration instanceOf() {
+    private String languageCode;
+    private String adminName;
+    private String adminEmail;
+
+    static public ApplicationConfiguration instanceOf() {
         if (instance == null) {
             instance = new ApplicationConfiguration();
         }
         return instance;
     }
-    
+
     public void initialize(PropertyLoader loader) throws IOException {
-        appLogger.info("Loading configuration");
+        appLogger.fine("Loading configuration");
         serverReadOnly = loader.loadBoolean(PropertyKeys.SERVER_READ_ONLY);
-        develMode = loader.loadBooleanFalseIfNullOrEmpty(PropertyKeys.DEVEL);
+        develMode = loader.loadBoolean(PropertyKeys.DEVEL);
+        languageCode = loader.loadString(PropertyKeys.LANGUAGE_CODE);
+        adminName = loader.loadStringOrNull(PropertyKeys.ADMIN_NAME);
+        adminEmail = loader.loadStringOrNull(PropertyKeys.ADMIN_EMAIL);
+        CountryCode.initialize(languageCode);
         Services.init(develMode);
     }
 
-    public Boolean isServerReadOnly(){
+    public Boolean isServerReadOnly() {
         return serverReadOnly;
     }
 
-    public Boolean isDevelMode(){
+    public Boolean isDevelMode() {
         return develMode;
+    }
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
     }
 }
