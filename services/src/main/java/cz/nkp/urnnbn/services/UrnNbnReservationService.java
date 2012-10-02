5888f5e700f48886e7618d47153d8f8021889c32
Martin.Rehanek <Martin.Rehanek@gmail.com>
2012-10-02 23:11:14 +0000

    services: fix for issue 35

--- services/src/main/java/cz/nkp/urnnbn/services/UrnNbnReservationService.java
+++ services/src/main/java/cz/nkp/urnnbn/services/UrnNbnReservationService.java
@@ -6,8 +6,9 @@ package cz.nkp.urnnbn.services;
 
 import cz.nkp.urnnbn.core.dto.Registrar;
 import cz.nkp.urnnbn.core.dto.UrnNbn;
-import cz.nkp.urnnbn.core.persistence.exceptions.DatabaseException;
+import cz.nkp.urnnbn.services.exceptions.AccessException;
 import cz.nkp.urnnbn.services.exceptions.UnknownRegistrarException;
+import cz.nkp.urnnbn.services.exceptions.UnknownUserException;
 import java.util.List;
 
 /**
@@ -17,32 +18,32 @@ import java.util.List;
 public interface UrnNbnReservationService extends BusinessService {
 
     /**
-     * This operation reserves certain number of urnNbns. UrnNbns that are reservec
-     * are not used for assigning by the application. The registrar is expected 
-     * to put those urnNbns into it's metadata records that are ready for import
-     * into the Resolver application. When one of those records is imported, 
-     * the application assignes urnNbn, that is present in import metadata, 
-     * to newly created  digital instance.
+     * This operation reserves certain number of urnNbns. UrnNbns that are
+     * reservec are not used for assigning by the application. The registrar is
+     * expected to put those urnNbns into it's metadata records that are ready
+     * for import into the Resolver application. When one of those records is
+     * imported, the application assignes urnNbn, that is present in import
+     * metadata, to newly created digital instance.
+     *
      * @param batchSize number of urnNbn identifiers returned
      * @param registrar
-     * @param userId Id of user to perform this operation.
+     * @param login Login of user to perform this operation.
      * @return Batch of urnNbn of size batchSize.
      */
-    public List<UrnNbn> reserveUrnNbnBatch(
-            int batchSize,
-            Registrar registrar,
-            long userId) throws DatabaseException;
+    //public List<UrnNbn> reserveUrnNbnBatch(int batchSize, Registrar registrar, long userId) 
+    public List<UrnNbn> reserveUrnNbnBatch(int batchSize, Registrar registrar, String login)
+            throws UnknownUserException, AccessException;
 
     /**
-     * 
+     *
      * @param registrarId
      * @return list of those urn:nbn, that are reserved
      */
     public List<UrnNbn> getReservedUrnNbnList(long registrarId)
-            throws DatabaseException, UnknownRegistrarException;
+            throws UnknownRegistrarException;
 
     /**
-     * 
+     *
      * @return Maximal size of batch that can be booked.
      */
     public int getMaxBatchSize();
