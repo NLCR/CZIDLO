96495ef3b76183c57da8a67995df9532910cffe8
Martin.Rehanek <Martin.Rehanek@gmail.com>
2013-01-02 07:39:01 +0000

    core: UrnNbnStatus now contains note that is used for predecessor note

--- core/src/main/java/cz/nkp/urnnbn/core/UrnNbnWithStatus.java
+++ core/src/main/java/cz/nkp/urnnbn/core/UrnNbnWithStatus.java
@@ -21,10 +21,12 @@ public class UrnNbnWithStatus {
     }
     private final UrnNbn urn;
     private final Status status;
+    private final String note;
 
-    public UrnNbnWithStatus(UrnNbn urn, Status status) {
+    public UrnNbnWithStatus(UrnNbn urn, Status status, String note) {
         this.urn = urn;
         this.status = status;
+        this.note = note;
     }
 
     public Status getStatus() {
@@ -33,5 +35,31 @@ public class UrnNbnWithStatus {
 
     public UrnNbn getUrn() {
         return urn;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.urn != null ? this.urn.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UrnNbnWithStatus other = (UrnNbnWithStatus) obj;
+        if (this.urn != other.urn && (this.urn == null || !this.urn.equals(other.urn))) {
+            return false;
+        }
+        return true;
     }
 }
