3869c0cdf5c270833b1a67578422b123f253b61a
Martin.Rehanek <Martin.Rehanek@gmail.com>
2013-01-17 04:07:40 +0000

    api: scripts fixed because of change  of urls (id -> registrarScopeIdentifier, identifiers -> registrarScopeIdentifiers) and some few other minor fixes in scripts

--- rest/src/main/resources/scripts/inc.sh
+++ rest/src/main/resources/scripts/inc.sh
@@ -8,6 +8,12 @@ rm $HEADERS_TMP
 rm $BODY_TMP
 }
 
+#autentizace při použití skriptů: prvni parametr funkce define_creditential() obsahuje název adresáře
+#tento adresář obsahuje následující soubory:
+#host - obsahuje doménu (případně port) stroje, na kterém běží Resolver
+#login - přihlašovací jméno
+#password - heslo
+#registrarCode - kód registrátora
 
 function define_creditentials() {
 HOST=`cat ${1}/host`;
