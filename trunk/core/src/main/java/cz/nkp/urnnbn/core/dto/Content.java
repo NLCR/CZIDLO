db9c75a8db8e68c2428fcac740b1d39cf9b1d922
xrosecky@gmail.com <xrosecky@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2013-05-20 19:39:58 +0000

    Content added

---
+++ trunk/core/src/main/java/cz/nkp/urnnbn/core/dto/Content.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.nkp.urnnbn.core.dto;
+
+/**
+ *
+ * @author xrosecky
+ */
+public class Content implements IdentifiableByLongAttribute {
+
+    private Long id;
+    private String language;
+    private String name;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Content other = (Content) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Content{" + "id=" + id + ", language=" + language + ", name=" + name + '}';
+    }
+
+}
