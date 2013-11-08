080be7c3f5667e755a74b5c2be2e2a54c46fd823
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2013-11-08 04:46:49 +0000

    services:

--- trunk/services/src/main/java/cz/nkp/urnnbn/services/impl/BusinessServiceImpl.java
+++ trunk/services/src/main/java/cz/nkp/urnnbn/services/impl/BusinessServiceImpl.java
@@ -17,61 +17,60 @@ import cz.nkp.urnnbn.services.exceptions.UnknownCatalogException;
 import cz.nkp.urnnbn.services.exceptions.UnknownDigDocException;
 import cz.nkp.urnnbn.services.exceptions.UnknownDigInstException;
 import cz.nkp.urnnbn.services.exceptions.UnknownDigLibException;
-import java.util.logging.Logger;
 
 /**
- *
+ * 
  * @author Martin Řehánek
  */
 abstract class BusinessServiceImpl implements BusinessService {
 
-    private static final Logger logger = Logger.getLogger(BusinessServiceImpl.class.getName());
-    final DAOFactory factory;
-    final AuthorizationModule authorization;
+	// private static final Logger logger = Logger.getLogger(BusinessServiceImpl.class.getName());
+	final DAOFactory factory;
+	final AuthorizationModule authorization;
 
-    public BusinessServiceImpl(DatabaseConnector conn) {
-        this(new DAOFactory(conn));
-    }
+	public BusinessServiceImpl(DatabaseConnector conn) {
+		this(new DAOFactory(conn));
+	}
 
-    public BusinessServiceImpl(DAOFactory factory) {
-        this.factory = factory;
-        authorization = new AuthorizationModule(factory);
-    }
+	public BusinessServiceImpl(DAOFactory factory) {
+		this.factory = factory;
+		authorization = new AuthorizationModule(factory);
+	}
 
-    long registrarOfDigLibrary(long libraryId) throws DatabaseException, UnknownDigLibException {
-        try {
-            DigitalLibrary lib = factory.diglLibDao().getLibraryById(libraryId);
-            return lib.getRegistrarId();
-        } catch (RecordNotFoundException ex) {
-            throw new UnknownDigLibException(libraryId);
-        }
-    }
+	long registrarOfDigLibrary(long libraryId) throws DatabaseException, UnknownDigLibException {
+		try {
+			DigitalLibrary lib = factory.diglLibDao().getLibraryById(libraryId);
+			return lib.getRegistrarId();
+		} catch (RecordNotFoundException ex) {
+			throw new UnknownDigLibException(libraryId);
+		}
+	}
 
-    long registrarOfCatalog(long catalogId) throws DatabaseException, UnknownCatalogException {
-        try {
-            Catalog catalog = factory.catalogDao().getCatalogById(catalogId);
-            return catalog.getRegistrarId();
-        } catch (RecordNotFoundException ex) {
-            throw new UnknownCatalogException(catalogId);
-        }
-    }
+	long registrarOfCatalog(long catalogId) throws DatabaseException, UnknownCatalogException {
+		try {
+			Catalog catalog = factory.catalogDao().getCatalogById(catalogId);
+			return catalog.getRegistrarId();
+		} catch (RecordNotFoundException ex) {
+			throw new UnknownCatalogException(catalogId);
+		}
+	}
 
-    long registrarOfDigInstance(long digInstanceId) throws DatabaseException, UnknownDigInstException {
-        try {
-            DigitalInstance instance = factory.digInstDao().getDigInstanceById(digInstanceId);
-            DigitalLibrary library = factory.diglLibDao().getLibraryById(instance.getLibraryId());
-            return library.getRegistrarId();
-        } catch (RecordNotFoundException ex) {
-            throw new UnknownDigInstException(digInstanceId);
-        }
-    }
+	long registrarOfDigInstance(long digInstanceId) throws DatabaseException, UnknownDigInstException {
+		try {
+			DigitalInstance instance = factory.digInstDao().getDigInstanceById(digInstanceId);
+			DigitalLibrary library = factory.diglLibDao().getLibraryById(instance.getLibraryId());
+			return library.getRegistrarId();
+		} catch (RecordNotFoundException ex) {
+			throw new UnknownDigInstException(digInstanceId);
+		}
+	}
 
-    long registrarOfDigDoc(long digDocId) throws DatabaseException, UnknownDigDocException {
-        try {
-            DigitalDocument digDoc = factory.documentDao().getDocumentByDbId(digDocId);
-            return digDoc.getRegistrarId();
-        } catch (RecordNotFoundException ex) {
-            throw new UnknownDigDocException(digDocId);
-        }
-    }
+	long registrarOfDigDoc(long digDocId) throws DatabaseException, UnknownDigDocException {
+		try {
+			DigitalDocument digDoc = factory.documentDao().getDocumentByDbId(digDocId);
+			return digDoc.getRegistrarId();
+		} catch (RecordNotFoundException ex) {
+			throw new UnknownDigDocException(digDocId);
+		}
+	}
 }
