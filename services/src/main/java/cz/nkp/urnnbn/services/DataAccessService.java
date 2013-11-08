f409cd684f614e960119a487df62895ce1f08f7f
Martin.Rehanek <Martin.Rehanek@gmail.com>
2013-11-08 04:46:49 +0000

    services:

--- services/src/main/java/cz/nkp/urnnbn/services/DataAccessService.java
+++ services/src/main/java/cz/nkp/urnnbn/services/DataAccessService.java
@@ -1,11 +1,19 @@
-        /*
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package cz.nkp.urnnbn.services;
 
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import org.joda.time.DateTime;
+
 import cz.nkp.urnnbn.core.RegistrarCode;
 import cz.nkp.urnnbn.core.RegistrarScopeIdType;
+import cz.nkp.urnnbn.core.UrnNbnExport;
+import cz.nkp.urnnbn.core.UrnNbnExportFilter;
 import cz.nkp.urnnbn.core.UrnNbnWithStatus;
 import cz.nkp.urnnbn.core.dto.Archiver;
 import cz.nkp.urnnbn.core.dto.Catalog;
@@ -21,127 +29,120 @@ import cz.nkp.urnnbn.core.dto.Registrar;
 import cz.nkp.urnnbn.core.dto.RegistrarScopeIdentifier;
 import cz.nkp.urnnbn.core.dto.SourceDocument;
 import cz.nkp.urnnbn.core.dto.UrnNbn;
-import cz.nkp.urnnbn.core.dto.UrnNbnExport;
 import cz.nkp.urnnbn.core.dto.User;
 import cz.nkp.urnnbn.services.exceptions.ContentNotFoundException;
 import cz.nkp.urnnbn.services.exceptions.NotAdminException;
 import cz.nkp.urnnbn.services.exceptions.RegistrarScopeIdentifierNotDefinedException;
 import cz.nkp.urnnbn.services.exceptions.UnknownUserException;
-import java.util.List;
-import java.util.Set;
-import java.util.logging.Logger;
-import org.joda.time.DateTime;
 
 /**
  * TODO: zbavit se DatabaseException (resp. přeložit na RuntimeException)
- *
+ * 
  * @author Martin Řehánek
  */
 public interface DataAccessService extends BusinessService {
 
-    static final Logger logger = Logger.getLogger(DataAccessService.class.getName());
+	static final Logger logger = Logger.getLogger(DataAccessService.class.getName());
 
-    public UrnNbnWithStatus urnByRegistrarCodeAndDocumentCode(RegistrarCode registrarCode, String documentCode, boolean withPredecessorsAndSuccessors);
+	public UrnNbnWithStatus urnByRegistrarCodeAndDocumentCode(RegistrarCode registrarCode, String documentCode,
+			boolean withPredecessorsAndSuccessors);
 
-    public DigitalDocument digDocByInternalId(long digDocId);
+	public DigitalDocument digDocByInternalId(long digDocId);
 
-    public UrnNbn urnByDigDocId(long id, boolean withPredecessorsAndSuccessors);
+	public UrnNbn urnByDigDocId(long id, boolean withPredecessorsAndSuccessors);
 
-    public List<RegistrarScopeIdentifier> registrarScopeIdentifiers(long digDocId);
+	public List<RegistrarScopeIdentifier> registrarScopeIdentifiers(long digDocId);
 
-    public RegistrarScopeIdentifier registrarScopeIdentifier(long digDocId, RegistrarScopeIdType type) throws RegistrarScopeIdentifierNotDefinedException;
+	public RegistrarScopeIdentifier registrarScopeIdentifier(long digDocId, RegistrarScopeIdType type)
+			throws RegistrarScopeIdentifierNotDefinedException;
 
-    public Registrar registrarById(long id);
+	public Registrar registrarById(long id);
 
-    public Archiver archiverById(long id);
+	public Archiver archiverById(long id);
 
-    public List<Archiver> archivers();
+	public List<Archiver> archivers();
 
-    public IntelectualEntity entityById(long id);
+	public IntelectualEntity entityById(long id);
 
-    public List<IntelectualEntity> entitiesByIdValue(String value);
-    
-    public List<IntelectualEntity> entitiesByIdValueWithFullTextSearch(String value);
+	public List<IntelectualEntity> entitiesByIdValue(String value);
 
-    public List<IntEntIdentifier> intEntIdentifiersByIntEntId(long intEntId);
+	public List<IntelectualEntity> entitiesByIdValueWithFullTextSearch(String value);
 
-    //pokud nenajde, vrati null
-    public Publication publicationByIntEntId(long intEntId);
+	public List<IntEntIdentifier> intEntIdentifiersByIntEntId(long intEntId);
 
-    //pokud nenajde, vrati null
-    public Originator originatorByIntEntId(long intEntId);
+	// pokud nenajde, vrati null
+	public Publication publicationByIntEntId(long intEntId);
 
-    //pokud nenajde, vrati null
-    public SourceDocument sourceDocumentByIntEntId(long intEntId);
+	// pokud nenajde, vrati null
+	public Originator originatorByIntEntId(long intEntId);
 
-    public Registrar registrarByCode(RegistrarCode code);
+	// pokud nenajde, vrati null
+	public SourceDocument sourceDocumentByIntEntId(long intEntId);
 
-    public List<DigitalLibrary> librariesByRegistrarId(long registrarId);
+	public Registrar registrarByCode(RegistrarCode code);
 
-    public List<Catalog> catalogsByRegistrarId(long registrarId);
+	public List<DigitalLibrary> librariesByRegistrarId(long registrarId);
 
-    public List<Catalog> catalogs();
+	public List<Catalog> catalogsByRegistrarId(long registrarId);
 
-    public List<Registrar> registrars();
+	public List<Catalog> catalogs();
 
-    public int digitalDocumentsCount(long registrarId);
+	public List<Registrar> registrars();
 
-    public DigitalDocument digDocByIdentifier(RegistrarScopeIdentifier id);
+	public int digitalDocumentsCount(long registrarId);
 
-    public List<DigitalDocument> digDocsOfIntEnt(long intEntId);
+	public DigitalDocument digDocByIdentifier(RegistrarScopeIdentifier id);
 
-    public long digitalInstancesCount();
+	public List<DigitalDocument> digDocsOfIntEnt(long intEntId);
 
-    public DigitalInstance digInstanceByInternalId(long id);
+	public long digitalInstancesCount();
 
-    public List<DigitalInstance> digInstancesByDigDocId(long digDocId);
+	public DigitalInstance digInstanceByInternalId(long id);
 
-    public DigitalLibrary libraryByInternalId(long libraryId);
+	public List<DigitalInstance> digInstancesByDigDocId(long digDocId);
 
-    /**
-     *
-     * @param login login of user performing this operation
-     * @param includePasswords
-     * @return
-     * @throws UnknownUserException
-     * @throws NotAdminException
-     */
-    public List<User> users(String login, boolean includePasswords)
-            throws UnknownUserException, NotAdminException;
+	public DigitalLibrary libraryByInternalId(long libraryId);
 
-    /**
-     *
-     * @param login login of user that is being lookuped
-     * @param includePassword
-     * @return
-     * @throws UnknownUserException
-     */
-    public User userByLogin(String login, boolean includePassword)
-            throws UnknownUserException;
+	/**
+	 * 
+	 * @param login
+	 *            login of user performing this operation
+	 * @param includePasswords
+	 * @return
+	 * @throws UnknownUserException
+	 * @throws NotAdminException
+	 */
+	public List<User> users(String login, boolean includePasswords) throws UnknownUserException, NotAdminException;
 
-    /**
-     *
-     * @param userId
-     * @param login login of user performing this operation
-     * @return
-     * @throws UnknownUserException
-     * @throws NotAdminException
-     */
-    public List<Registrar> registrarsManagedByUser(long userId, String login)
-            throws UnknownUserException, NotAdminException;
+	/**
+	 * 
+	 * @param login
+	 *            login of user that is being lookuped
+	 * @param includePassword
+	 * @return
+	 * @throws UnknownUserException
+	 */
+	public User userByLogin(String login, boolean includePassword) throws UnknownUserException;
 
-    public Set<UrnNbn> urnNbnsOfChangedRecords(DateTime from, DateTime until);
+	/**
+	 * 
+	 * @param userId
+	 * @param login
+	 *            login of user performing this operation
+	 * @return
+	 * @throws UnknownUserException
+	 * @throws NotAdminException
+	 */
+	public List<Registrar> registrarsManagedByUser(long userId, String login) throws UnknownUserException, NotAdminException;
 
-    public Set<UrnNbn> urnNbnsOfChangedRecordsOfRegistrar(Registrar registrar, DateTime from, DateTime until);
+	public Set<UrnNbn> urnNbnsOfChangedRecords(DateTime from, DateTime until);
 
-    public List<UrnNbn> urnNbnsOfRegistrar(RegistrarCode registarCode);
-    
-    public Content contentByNameAndLanguage(String name, String language) throws ContentNotFoundException;
-    
-    //FIXME: move filter fields to separate class
-    public List<UrnNbnExport> selectByCriteria(DateTime begin, DateTime end,
-			List<String> registrars, String registrationMode,
-			List<String> entityTypes, Boolean cnbAssigned, Boolean issnAsigned,
-			Boolean isbnAssigned, Boolean active);
-    
+	public Set<UrnNbn> urnNbnsOfChangedRecordsOfRegistrar(Registrar registrar, DateTime from, DateTime until);
+
+	public List<UrnNbn> urnNbnsOfRegistrar(RegistrarCode registarCode);
+
+	public Content contentByNameAndLanguage(String name, String language) throws ContentNotFoundException;
+
+	public List<UrnNbnExport> selectByCriteria(String languageCode, UrnNbnExportFilter filter, boolean withDigitalInstances);
+
 }
