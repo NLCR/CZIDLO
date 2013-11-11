9e6ebe4320946fd797b519ae76563fb5a22572fe
Martin.Rehanek <Martin.Rehanek@gmail.com>
2013-11-11 02:57:08 +0000

    persistence: pom.xml cleared - maven-assembly-plugin removed (no need for persistence-jar-with-dependencies.jar), maven-compiler-plugin upgraded to version 3.1, Module version upgraded to 4.0; nbactions.xml removed; Database for build tests renamed.

--- persistence/src/test/java/cz/nkp/urnnbn/core/persistence/impl/postgres/DatabaseConfig.java
+++ persistence/src/test/java/cz/nkp/urnnbn/core/persistence/impl/postgres/DatabaseConfig.java
@@ -5,15 +5,15 @@
 package cz.nkp.urnnbn.core.persistence.impl.postgres;
 
 /**
- *
+ * 
  * @author Martin Řehánek
  */
 public class DatabaseConfig {
 
-    public static String DRIVER = "org.postgresql.Driver";
-    public static String HOST = "localhost";
-    public static String DATABASE = "resolvertestpersistence";
-    public static int PORT = 5432;
-    public static String LOGIN = "testuser";
-    public static String PASSWORD = "testpass";
+	public static String DRIVER = "org.postgresql.Driver";
+	public static String HOST = "localhost";
+	public static String DATABASE = "czidlo_core_junit_test";
+	public static int PORT = 5432;
+	public static String LOGIN = "testuser";
+	public static String PASSWORD = "testpass";
 }
