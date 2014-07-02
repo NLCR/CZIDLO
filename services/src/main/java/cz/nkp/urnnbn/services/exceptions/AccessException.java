65498c3acc7e097de94c3be5f51b3148143d5714
Martin.Rehanek <Martin.Rehanek@gmail.com>
2014-07-02 18:05:43 +0000

    services: message for API error NOT_AUTHORIZED slightly changed

--- services/src/main/java/cz/nkp/urnnbn/services/exceptions/AccessException.java
+++ services/src/main/java/cz/nkp/urnnbn/services/exceptions/AccessException.java
@@ -5,26 +5,27 @@
 package cz.nkp.urnnbn.services.exceptions;
 
 import cz.nkp.urnnbn.core.RegistrarCode;
-    
+
 /**
- *
+ * 
  * @author Martin Řehánek
  */
 public class AccessException extends Exception {
 
-    private final String login;
+	private static final long serialVersionUID = -3532839302495659473L;
+	private final String login;
 
-    public AccessException(String login, RegistrarCode registrarCode) {
-        super("user " + login + " not allowed to manage registrar with code " + registrarCode);
-        this.login = login;
-    }
+	public AccessException(String login, RegistrarCode registrarCode) {
+		super("user '" + login + "' not allowed to manage registrar with code '" + registrarCode + "'");
+		this.login = login;
+	}
 
-    public AccessException(String login, long registrarId) {
-        super("user " + login + " not allowed to manage registrar with id " + registrarId);
-        this.login = login;
-    }
+	public AccessException(String login, long registrarId) {
+		super("user '" + login + "' not allowed to manage registrar with id " + registrarId);
+		this.login = login;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public String getLogin() {
+		return login;
+	}
 }
