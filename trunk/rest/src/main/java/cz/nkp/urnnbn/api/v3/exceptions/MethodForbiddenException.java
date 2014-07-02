a8399556ebc54ed0ce03f1015630ed3ee5f2b364
Martin.Rehanek@gmail.com <Martin.Rehanek@gmail.com@fc89f25d-48f4-a428-907d-55b214044a8b>
2014-07-02 18:31:04 +0000

    api: urn:nbn deactivation implementation

--- trunk/rest/src/main/java/cz/nkp/urnnbn/api/v3/exceptions/MethodForbiddenException.java
+++ trunk/rest/src/main/java/cz/nkp/urnnbn/api/v3/exceptions/MethodForbiddenException.java
@@ -7,12 +7,14 @@ package cz.nkp.urnnbn.api.v3.exceptions;
 import javax.ws.rs.core.Response.Status;
 
 /**
- *
+ * 
  * @author Martin Řehánek
  */
 public class MethodForbiddenException extends ApiV3Exception {
 
-    public MethodForbiddenException() {
-        super(Status.FORBIDDEN, "FORBIDDEN", "This method is (temporarily) not allowed");
-    }
+	private static final long serialVersionUID = -5047481327211239231L;
+
+	public MethodForbiddenException() {
+		super(Status.FORBIDDEN, "FORBIDDEN", "This method is (temporarily) not allowed");
+	}
 }
