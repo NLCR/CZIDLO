bfcc1198b08b1c79fdc82a70518f59e6b5bc127d
Martin.Rehanek <Martin.Rehanek@gmail.com>
2014-07-02 18:31:04 +0000

    api: urn:nbn deactivation implementation

--- rest/src/main/java/cz/nkp/urnnbn/api/AbstractUrnNbnResource.java
+++ rest/src/main/java/cz/nkp/urnnbn/api/AbstractUrnNbnResource.java
@@ -20,15 +20,20 @@ import cz.nkp.urnnbn.core.UrnNbnWithStatus;
 import cz.nkp.urnnbn.core.dto.UrnNbn;
 
 /**
- *
+ * 
  * @author Martin Řehánek
  */
 public abstract class AbstractUrnNbnResource extends Resource {
-    
-    public abstract String getUrnNbnXmlRecord(String urnNbnString); 
 
-    protected final UrnNbnWithStatus getUrnNbnWithStatus(String urnNbnString) {
-        UrnNbn urnParsed = Parser.parseUrn(urnNbnString);
-        return dataAccessService().urnByRegistrarCodeAndDocumentCode(urnParsed.getRegistrarCode(), urnParsed.getDocumentCode(), true);
-    }
+	public abstract String getUrnNbnXmlRecord(String urnNbnString);
+
+	protected final UrnNbnWithStatus getUrnNbnWithStatus(String urnNbnString) {
+		UrnNbn urnParsed = Parser.parseUrn(urnNbnString);
+		return dataAccessService().urnByRegistrarCodeAndDocumentCode(urnParsed.getRegistrarCode(), urnParsed.getDocumentCode(), true);
+	}
+
+	protected final UrnNbnWithStatus getUrnNbnWithStatus(UrnNbn urn) {
+		return dataAccessService().urnByRegistrarCodeAndDocumentCode(urn.getRegistrarCode(), urn.getDocumentCode(), true);
+	}
+
 }
---
+++ rest/src/main/java/cz/nkp/urnnbn/api/v3/exceptions/IncorrectUrnStateException.java
@@ -0,0 +1,18 @@
+package cz.nkp.urnnbn.api.v3.exceptions;
+
+import javax.ws.rs.core.Response.Status;
+import cz.nkp.urnnbn.core.UrnNbnWithStatus;
+
+/**
+ * 
+ * @author Martin Řehánek
+ */
+public class IncorrectUrnStateException extends ApiV3Exception {
+
+	private static final long serialVersionUID = -3179637427845166321L;
+
+	public IncorrectUrnStateException(UrnNbnWithStatus urnNbnWithStatus) {
+		super(Status.FORBIDDEN, "INCORRECT_URN_NBN_STATE", urnNbnWithStatus.getUrn().toString() + ": "
+				+ urnNbnWithStatus.getStatus().toString());
+	}
+}
