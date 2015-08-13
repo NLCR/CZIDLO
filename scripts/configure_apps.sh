79fd13c7fbd71bfd408630550a239063ee2ba230
rzeh4n <Martin.Rehanek@gmail.com>
2015-08-13 12:11:26 +0200

    Extend script to update configuration with hibernate.conf and context replacement.

--- scripts/configure_apps.sh
+++ scripts/configure_apps.sh
@@ -2,21 +2,47 @@
 # Script for replacing modules' configuration from backup
 # after some apps are redeployed.
 
-BAK_DIR=/home/martin/software/apache-tomcat-8.0.24/bak/
+BAK_DIR=/home/martin/software/apache-tomcat-8.0.24/bak
 TOMCAT_HOME=/home/martin/software/apache-tomcat-8.0.24
 
 function replace_prop {
 	APP=$1
-	PROP_BAK=$BAK_DIR/$APP.properties
-	PROP_DEPLOYED=$TOMCAT_HOME/webapps/$APP/WEB-INF/classes/$APP.properties
-	echo "loading configuration properties for app $MODULE"
-	cp $PROP_BAK $PROP_DEPLOYED
+	echo "$APP: updating configuration properties"
+	FILE_BAK=$BAK_DIR/$APP.properties
+	FILE_DEPLOYED=$TOMCAT_HOME/webapps/$APP/WEB-INF/classes/$APP.properties
+	cp $FILE_BAK $FILE_DEPLOYED
 }
 
-echo "Replacing configuration properties from backup:"
-replace_prop web
-replace_prop api
-replace_prop processDataServer
-replace_prop oaiPmhProvider
+function replace_context {
+	APP=$1
+	echo "$APP: updating context"
+	FILE_BAK=$BAK_DIR/$APP.xml
+	FILE_DEPLOYED=$TOMCAT_HOME/conf/Catalina/localhost/$APP.xml
+	cp $FILE_BAK $FILE_DEPLOYED
+}
 
-#TODO: update context from backup
+function replace_hibernate_conf {
+	APP=$1
+        echo "$APP: updating hibernate.conf"
+        FILE_BAK=$BAK_DIR/$APP-hibernate.cfg.xml
+        FILE_DEPLOYED=$TOMCAT_HOME/webapps/$APP/WEB-INF/classes/hibernate.cfg.xml
+        cp $FILE_BAK $FILE_DEPLOYED
+}
+
+echo "Replacing configuration from backup:"
+
+replace_prop web
+replace_context web
+replace_hibernate_conf web
+
+replace_prop processDataServer
+replace_context processDataServer
+replace_hibernate_conf processDataServer
+
+replace_prop api
+replace_context api
+
+replace_prop oaiPmhProvider
+replace_context oaiPmhProvider
+
+
