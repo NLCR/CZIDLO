da9ef9344e5210b85db5b9a96bfa3e3fc68fbfb3
rzeh4n <Martin.Rehanek@gmail.com>
2016-02-16 18:28:39 +0100

    added api v3 tests for digital instances

--- api/src/test/java/cz/nkp/urnnbn/api/v3/ApiV3Tests.java
+++ api/src/test/java/cz/nkp/urnnbn/api/v3/ApiV3Tests.java
@@ -20,12 +20,15 @@ import static com.jayway.restassured.RestAssured.with;
 import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
 import static com.jayway.restassured.config.XmlConfig.xmlConfig;
 import static com.jayway.restassured.path.xml.config.XmlPathConfig.xmlPathConfig;
+import static com.jayway.restassured.config.RedirectConfig.redirectConfig;
+import static com.jayway.restassured.config.SSLConfig.sslConfig;
 
 import java.util.Random;
 
 import javax.xml.namespace.NamespaceContext;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.config.RedirectConfig;
 import com.jayway.restassured.config.RestAssuredConfig;
 import com.jayway.restassured.path.xml.XmlPath;
 import com.jayway.restassured.path.xml.config.XmlPathConfig;
@@ -36,10 +39,12 @@ public abstract class ApiV3Tests {
 
     private static final String BASE_URI = "http://localhost";
     private static final int PORT = 8080;
+    private static final String BASE_PATH = "/api/v3";
+    static final String HTTPS_API_URL = "https://localhost:8443" + BASE_PATH;
+
     private static final String RESPONSE_NS = "http://resolver.nkp.cz/v3/";
     private static final String RESPONSE_NS_PREFIX = "c";
     private static final String RESPONSE_XSD = "http://localhost:8080/api/v3/response.xsd";
-    private static final String BASE_PATH = "/api/v3/";
 
     Random rand = new Random();
     String responseXsdString;
@@ -49,6 +54,9 @@ public abstract class ApiV3Tests {
         RestAssured.baseURI = BASE_URI;
         RestAssured.port = PORT;
         RestAssured.basePath = BASE_PATH;
+        // doesn't work, must use .relaxedHTTPSValidation() in each request
+        // see https://github.com/jayway/rest-assured/issues/561
+        RestAssured.useRelaxedHTTPSValidation();
         // RestAssured.authentication = basic("username", "password");
         // RestAssured.rootPath = "x.y.z";
         responseXsdString = Utils.readXsd(RESPONSE_XSD);
@@ -57,7 +65,12 @@ public abstract class ApiV3Tests {
     }
 
     RestAssuredConfig namespaceAwareXmlConfig() {
-        return newConfig().xmlConfig(xmlConfig().with().namespaceAware(true).declareNamespace(RESPONSE_NS_PREFIX, RESPONSE_NS));
+        return newConfig().xmlConfig(xmlConfig().with().namespaceAware(true).declareNamespace(RESPONSE_NS_PREFIX, RESPONSE_NS))//
+                .sslConfig(sslConfig().relaxedHTTPSValidation())//
+                // https + redirections doesn't work
+                // see https://github.com/jayway/rest-assured/issues/467
+                .redirect(new RedirectConfig(true, false, false, 10))//
+        ;
     }
 
     XmlPathConfig namespaceAwareXmlpathConfig() {
--- api/src/test/java/cz/nkp/urnnbn/api/v3/DigitalInstanceTests.java
+++ api/src/test/java/cz/nkp/urnnbn/api/v3/DigitalInstanceTests.java
@@ -4,7 +4,7 @@ import static com.jayway.restassured.RestAssured.expect;
 import static com.jayway.restassured.RestAssured.with;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.matchesXsd;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.hasXPath;//.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasXPath;
 import static org.junit.Assert.assertThat;
 
 import java.util.logging.Logger;
@@ -19,8 +19,20 @@ import com.jayway.restassured.path.xml.XmlPath;
 public class DigitalInstanceTests extends ApiV3Tests {
 
     private static final Logger LOGGER = Logger.getLogger(DigitalInstanceTests.class.getName());
-    private static final int ID = 5;// TODO: ziskat existujici
-    private static final int UNKNOWN_ID = 9999999;// should be unassigned in testing instance of czidlo
+    private static final String TEST_USER_LOGIN = "martin";
+    private static final String TEST_USER_PASSWORD = "i0oEhu";
+    private static final String ID_INVALID = "abc";
+    private static final int ID_UNKNOWN = 9999999;// make sure it exists and is not assigned
+    private static final int ID_ACTIVE = 27703;// make sure it exists and is active
+    private static final int ID_DEACTIVATED = 60;// make sure it exists and is not active
+    private static final int ID_ACTIVE_NO_RIGHTS = 27703;// make sure it exists, is active and TEST_USER has no access rights to it
+    // select diginst.id as diginstid from
+    // (select id from registrar where code='tst01') as registrar join
+    // (select id, registrarId from digitallibrary) as diglib on registrar.id=diglib.registrarid join
+    // (select id, digitallibraryid from digitalinstance where active='true') as diginst on diglib.id=diginst.digitallibraryid;
+    // and if needed to re-run tests:
+    // update digitalinstance set active='t' where id=642390;
+    private static final Integer ID_ACTIVE_RIGHTS = null;// 642390;// make sure it exists, is active and TEST_USER has access rights to it
 
     @BeforeSuite
     public void beforeSuite() {
@@ -34,41 +46,41 @@ public class DigitalInstanceTests extends ApiV3Tests {
                 .body(matchesXsd(responseXsdString))//
                 .body(hasXPath("/c:response/c:error/c:message", nsContext))//
                 .body(hasXPath("/c:response/c:error/c:code", nsContext))//
-                .when().get("/digitalInstances/id/abc").andReturn().asString();
+                .when().get("/digitalInstances/id/" + ID_INVALID).andReturn().asString();
         XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:error");
         Assert.assertEquals(xmlPath.getString("c:code"), "INVALID_DIGITAL_INSTANCE_ID");
     }
 
     @Test
-    public void getDigitalInstanceByInvalidId() {
+    public void getDigitalInstanceByUnknownId() {
         String xml = with().config(namespaceAwareXmlConfig()).expect()//
                 .statusCode(404).contentType(ContentType.XML)//
                 .body(matchesXsd(responseXsdString))//
                 .body(hasXPath("/c:response/c:error/c:message", nsContext))//
                 .body(hasXPath("/c:response/c:error/c:code", nsContext))//
-                .when().get("/digitalInstances/id/" + UNKNOWN_ID).andReturn().asString();
+                .when().get("/digitalInstances/id/" + ID_UNKNOWN).andReturn().asString();
         XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:error");
         Assert.assertEquals(xmlPath.getString("c:code"), "UNKNOWN_DIGITAL_INSTANCE");
     }
 
     @Test
     public void getExistingDigitalInstanceStatusCode() {
-        expect().statusCode(200).when().get("/digitalInstances/id/" + ID);
+        expect().statusCode(200).when().get("/digitalInstances/id/" + ID_ACTIVE);
     }
 
     @Test
     public void getExistingDigitalInstanceContentType() {
-        expect().contentType(ContentType.XML).when().get("/digitalInstances/id/" + ID);
+        expect().contentType(ContentType.XML).when().get("/digitalInstances/id/" + ID_ACTIVE);
     }
 
     @Test
     public void getDigitalInstanceValidByXsd() {
-        expect().body(matchesXsd(responseXsdString)).when().get("/digitalInstances/id/" + ID);
+        expect().body(matchesXsd(responseXsdString)).when().get("/digitalInstances/id/" + ID_ACTIVE);
     }
 
     @Test
     public void getDigitalInstancesData() {
-        with().config(namespaceAwareXmlConfig()).when().get("/digitalInstances/id/" + ID).then()//
+        with().config(namespaceAwareXmlConfig()).when().get("/digitalInstances/id/" + ID_ACTIVE).then()//
                 .assertThat().body(hasXPath("/c:response", nsContext))//
                 .assertThat().body(hasXPath("/c:response/c:digitalInstance", nsContext))//
                 .assertThat().body(hasXPath("/c:response/c:digitalInstance/@id", nsContext))//
@@ -87,18 +99,84 @@ public class DigitalInstanceTests extends ApiV3Tests {
     }
 
     @Test
-    public void deactivateDigitalInstancesNotAuthenticated() {
-        //TODO: body seems to be empty
+    public void getDigitalInstanceDeactivatedActivity() {
         String xml = with().config(namespaceAwareXmlConfig()).expect()//
+                .statusCode(200).contentType(ContentType.XML)//
+                .body(matchesXsd(responseXsdString))//
+                .when().get("/digitalInstances/id/" + ID_DEACTIVATED).andReturn().asString();
+        XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:digitalInstance");
+        Assert.assertEquals(xmlPath.getBoolean("@active"), false);
+    }
+
+    public void getDigitalInstanceActiveActivity() {
+        String xml = with().config(namespaceAwareXmlConfig()).expect()//
+                .statusCode(200).contentType(ContentType.XML)//
+                .body(matchesXsd(responseXsdString))//
+                .when().get("/digitalInstances/id/" + ID_ACTIVE).andReturn().asString();
+        XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:digitalInstance");
+        Assert.assertEquals(xmlPath.getBoolean("@active"), true);
+    }
+
+    @Test
+    public void deactivateDigitalInstanceNotAuthenticated() {
+        // TODO: check this out, seems to be the only error when response is not xml with error code
+        with().config(namespaceAwareXmlConfig())//
+                .expect().statusCode(401)//
+                .when().delete(HTTPS_API_URL + "/digitalInstances/id/" + ID_ACTIVE_NO_RIGHTS)//
+        // .when().delete("/digitalInstances/id/" + ID_ACTIVE)//
+        ;
+    }
+
+    @Test
+    public void deactivateDigitalInstanceNotAuthorized() {
+        String xml = with().config(namespaceAwareXmlConfig()).auth().basic(TEST_USER_LOGIN, TEST_USER_PASSWORD)//
+                .expect()//
                 .statusCode(401).contentType(ContentType.XML)//
                 .body(matchesXsd(responseXsdString))//
                 .body(hasXPath("/c:response/c:error/c:message", nsContext))//
                 .body(hasXPath("/c:response/c:error/c:code", nsContext))//
-                .when().delete("/digitalInstances/id/" + ID).andReturn().asString();
+                .when().delete(HTTPS_API_URL + "/digitalInstances/id/" + ID_ACTIVE_NO_RIGHTS)//
+                // .when().delete("/digitalInstances/id/" + ID_ACTIVE)//
+                .andReturn().asString();
         XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:error");
-        Assert.assertEquals(xmlPath.getString("c:code"), "NOT_AUTHENTICATED");
+        Assert.assertEquals(xmlPath.getString("c:code"), "NOT_AUTHORIZED");
     }
-    
-    //TODO: errors NOT_AUTHORIZED, ALREADY_DEACTIVATED
 
+    @Test
+    public void deactivateDigitalInstanceAlreadyDeactivated() {
+        String xml = with().config(namespaceAwareXmlConfig()).auth().basic(TEST_USER_LOGIN, TEST_USER_PASSWORD)//
+                .expect()//
+                .statusCode(403).contentType(ContentType.XML)//
+                .body(matchesXsd(responseXsdString))//
+                .body(hasXPath("/c:response/c:error/c:message", nsContext))//
+                .body(hasXPath("/c:response/c:error/c:code", nsContext))//
+                .when().delete(HTTPS_API_URL + "/digitalInstances/id/" + ID_DEACTIVATED)
+                // .when().delete("/digitalInstances/id/" + ID_ACTIVE)
+                .andReturn().asString();
+        XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:error");
+        Assert.assertEquals(xmlPath.getString("c:code"), "ALREADY_DEACTIVATED");
+    }
+
+    @Test
+    public void deactivateDigitalInstance() {
+        if (ID_ACTIVE_RIGHTS != null) {
+            String xml = with().config(namespaceAwareXmlConfig()).auth().basic(TEST_USER_LOGIN, TEST_USER_PASSWORD)//
+                    .expect()//
+                    .statusCode(200).contentType(ContentType.XML)//
+                    .body(matchesXsd(responseXsdString))//
+                    .body(hasXPath("/c:response/c:digitalInstance/c:url", nsContext))//
+                    .body(hasXPath("/c:response/c:digitalInstance/c:created", nsContext))//
+                    .body(hasXPath("/c:response/c:digitalInstance/c:deactivated", nsContext))//
+                    // TODO: in api V4 should be <digitalLibrary id='12'> just like in GET. Now it is <digitalLibraryId>12</digitalLibraryId>
+                    .body(hasXPath("/c:response/c:digitalInstance/c:digitalLibraryId", nsContext))//
+                    .when().delete(HTTPS_API_URL + "/digitalInstances/id/" + ID_ACTIVE_RIGHTS)
+                    // .when().delete("/digitalInstances/id/" + ID_ACTIVE)
+                    .andReturn().asString();
+            XmlPath xmlPath = XmlPath.from(xml).using(namespaceAwareXmlpathConfig()).setRoot("c:response.c:digitalInstance");
+            Assert.assertEquals((Integer) xmlPath.getInt("@id"), ID_ACTIVE_RIGHTS);
+            Assert.assertEquals(xmlPath.getBoolean("c:active"), false);
+        } else {
+            LOGGER.info("ignoring test deactivateDigitalInstance - no id of digital instance to be deactivated is available");
+        }
+    }
 }
