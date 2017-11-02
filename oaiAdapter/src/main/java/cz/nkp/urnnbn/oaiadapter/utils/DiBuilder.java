8f5aa06a011c1c1c6d765968bad173ab9122a27e
Martin Řehánek <Martin.Rehanek@gmail.com>
2017-11-02 17:37:29 +0100

    fixed DiBuilder

--- oaiAdapter/src/main/java/cz/nkp/urnnbn/oaiadapter/utils/DiBuilder.java
+++ oaiAdapter/src/main/java/cz/nkp/urnnbn/oaiadapter/utils/DiBuilder.java
@@ -36,54 +36,29 @@ public class DiBuilder {
         return di;
     }
 
-    public static DigitalInstance buildDiFromGetDigitalInstanceByLibraryIdResponse(Document responseDoc) {
-        DigitalInstance di = new DigitalInstance();
-        Nodes libraryIdNodes = responseDoc.query("//r:digitalInstance/r:digitalLibrary/@id", CzidloApiConnector.CONTEXT);
-        if (libraryIdNodes.size() == 1) {
-            di.setLibraryId(Long.valueOf(libraryIdNodes.get(0).getValue()));
-        }
-        Nodes urlNodes = responseDoc.query("//r:digitalInstance/r:url", CzidloApiConnector.CONTEXT);
-        if (urlNodes.size() == 1) {
-            di.setUrl(urlNodes.get(0).getValue());
-        }
-        Nodes formatNodes = responseDoc.query("//r:digitalInstance/r:format", CzidloApiConnector.CONTEXT);
-        if (formatNodes.size() == 1) {
-            di.setFormat(formatNodes.get(0).getValue());
-        }
-        Nodes accessibilityNodes = responseDoc.query("//r:digitalInstance/r:accessibility", CzidloApiConnector.CONTEXT);
-        if (accessibilityNodes.size() == 1) {
-            di.setAccessibility(accessibilityNodes.get(0).getValue());
-        }
-        Nodes idNodes = responseDoc.query("//r:digitalInstance/@id", CzidloApiConnector.CONTEXT);
-        if (idNodes.size() == 1) {
-            di.setId(Long.valueOf(idNodes.get(0).getValue()));
-        }
-        return di;
-    }
-
     public static List<DigitalInstance> buildDisFromGetDigitalInstancesByUrnNbn(Document responseDoc) {
-        Nodes diNodes = responseDoc.query("/r:response/r:digitalInstances/r:digialInstance", CzidloApiConnector.CONTEXT);
+        Nodes diNodes = responseDoc.query("/r:response/r:digitalInstances/r:digitalInstance", CzidloApiConnector.CONTEXT);
         List<DigitalInstance> result = new ArrayList<>(diNodes.size());
-        for (int i = 0; i < result.size(); i++) {
+        for (int i = 0; i < diNodes.size(); i++) {
             DigitalInstance di = new DigitalInstance();
             Element diEl = (Element) diNodes.get(i);
             di.setId(Long.valueOf(diEl.getAttributeValue("id")));
-            di.setLibraryId(Long.valueOf(diEl.query("digitalLibraryId").get(0).getValue()));
-            di.setUrl(diEl.query("url").get(0).getValue());
+            di.setLibraryId(Long.valueOf(diEl.query("r:digitalLibraryId", CzidloApiConnector.CONTEXT).get(0).getValue()));
+            di.setUrl(diEl.query("r:url", CzidloApiConnector.CONTEXT).get(0).getValue());
             di.setActive(Boolean.valueOf(diEl.getAttributeValue("active")));
-            Nodes formatNodes = diEl.query("format");
+            Nodes formatNodes = diEl.query("r:format", CzidloApiConnector.CONTEXT);
             if (formatNodes.size() > 0) {
                 di.setFormat(formatNodes.get(0).getValue().trim());
             }
-            Nodes accessibilityNodes = diEl.query("accessibility");
+            Nodes accessibilityNodes = diEl.query("r:accessibility", CzidloApiConnector.CONTEXT);
             if (accessibilityNodes.size() > 0) {
                 di.setAccessibility(accessibilityNodes.get(0).getValue().trim());
             }
-            Nodes createdNodes = diEl.query("created");
+            Nodes createdNodes = diEl.query("r:created", CzidloApiConnector.CONTEXT);
             if (createdNodes.size() > 0) {
                 di.setCreated(DateTime.parse(createdNodes.get(0).getValue()));
             }
-            Nodes deactivatedNodes = diEl.query("deactivated");
+            Nodes deactivatedNodes = diEl.query("r:deactivated", CzidloApiConnector.CONTEXT);
             if (deactivatedNodes.size() > 0) {
                 di.setDeactivated(DateTime.parse(deactivatedNodes.get(0).getValue()));
             }
@@ -91,6 +66,5 @@ public class DiBuilder {
         }
         return result;
     }
-
 
 }
