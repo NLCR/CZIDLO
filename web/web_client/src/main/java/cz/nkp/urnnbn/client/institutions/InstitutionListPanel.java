f4c60346dcdaea83799afeba61187f32a406b83b
Martin Řehánek <Martin.Rehanek@gmail.com>
2017-11-27 18:03:45 +0100

    cleanup

--- web/web_client/src/main/java/cz/nkp/urnnbn/client/institutions/InstitutionListPanel.java
+++ web/web_client/src/main/java/cz/nkp/urnnbn/client/institutions/InstitutionListPanel.java
@@ -24,7 +24,10 @@ import cz.nkp.urnnbn.shared.dto.ArchiverDTO;
 import cz.nkp.urnnbn.shared.dto.RegistrarDTO;
 import cz.nkp.urnnbn.shared.dto.UserDTO;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class InstitutionListPanel extends VerticalPanel {
@@ -38,25 +41,6 @@ public class InstitutionListPanel extends VerticalPanel {
     private final UserDTO user;
     private ArrayList<RegistrarDTO> registrars = new ArrayList<>(0);
     private ArrayList<ArchiverDTO> archivers = new ArrayList<>(0);
-
-    private static class CustomHTML<T> extends HTML {
-
-        private T object;
-
-        public CustomHTML(String html, T object) {
-            super(html);
-            this.object = object;
-        }
-
-        public T getObject() {
-            return object;
-        }
-
-        public void setObject(T object) {
-            this.object = object;
-        }
-
-    }
 
     public InstitutionListPanel(InstitutionsAdminstrationTab superPanel, UserDTO user) {
         this.superPanel = superPanel;
@@ -75,7 +59,7 @@ public class InstitutionListPanel extends VerticalPanel {
                 if (!user.isSuperAdmin()) {
                     result = removeHidden(result);
                 }
-                registrars = sortByName(result);
+                registrars = result;
                 reload();
             }
 
@@ -95,17 +79,6 @@ public class InstitutionListPanel extends VerticalPanel {
         return result;
     }
 
-    private <T extends ArchiverDTO> ArrayList<T> sortByName(ArrayList<T> result) {
-        Collections.sort(result, new Comparator<ArchiverDTO>() {
-            private final CzechStringComparator stringComparator = new CzechStringComparator();
-
-            public int compare(ArchiverDTO o1, ArchiverDTO o2) {
-                return stringComparator.compare(o1.getName(), o2.getName());
-            }
-        });
-        return result;
-    }
-
     void loadArchivers() {
         institutionsService.getAllArchivers(new AsyncCallback<ArrayList<ArchiverDTO>>() {
 
@@ -114,7 +87,7 @@ public class InstitutionListPanel extends VerticalPanel {
                 if (!user.isSuperAdmin()) {
                     result = removeHidden(result);
                 }
-                archivers = sortByName(result);
+                archivers = result;
                 reload();
             }
 
@@ -135,9 +108,16 @@ public class InstitutionListPanel extends VerticalPanel {
         VerticalPanel result = new VerticalPanel();
         result.setStyleName(css.block());
         result.add(registrarsHeading());
-        result.add(buildRegistrarsGrid());
+        result.add(buildRegistrarsGrid(registrars));
         if (user.isSuperAdmin()) {
-            result.add(addRegistrarButton());
+            result.add(
+                    new Button(constants.add(), new ClickHandler() {
+
+                        @Override
+                        public void onClick(ClickEvent event) {
+                            addRegistrar();
+                        }
+                    }));
         }
         return result;
     }
@@ -146,9 +126,16 @@ public class InstitutionListPanel extends VerticalPanel {
         VerticalPanel result = new VerticalPanel();
         result.setStyleName(css.block());
         result.add(archiversHeading());
-        result.add(buildArchiversGrid());
+        result.add(buildArchiversGrid(archivers));
         if (user.isSuperAdmin()) {
-            result.add(addArchiverButton());
+            result.add(
+                    new Button(constants.add(), new ClickHandler() {
+
+                        @Override
+                        public void onClick(ClickEvent event) {
+                            addArchiver();
+                        }
+                    }));
         }
         return result;
     }
@@ -159,103 +146,13 @@ public class InstitutionListPanel extends VerticalPanel {
         return label;
     }
 
-    @Deprecated
-    private Button registrarDeleteButton(final RegistrarDTO registrar) {
-        return new Button(constants.delete(), new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent clickEvent) {
-                deleteRegistrar(registrar);
-            }
-        });
-    }
-
-    private void deleteRegistrar(final RegistrarDTO registrar) {
-        if (Window.confirm(messages.confirmDeleteRegistrar(registrar.getName()))) {
-            institutionsService.deleteRegistrar(registrar, new AsyncCallback<Void>() {
-
-                @Override
-                public void onSuccess(Void result) {
-                    registrars.remove(registrar);
-                    reload();
-                }
-
-                @Override
-                public void onFailure(Throwable caught) {
-                    Window.alert(messages.registrarCannotBeDeleted(registrar.getName()) + ": " + caught.getMessage());
-                }
-            });
-        }
-    }
-
-    @Deprecated
-    private Button registrarDetailsButton(final RegistrarDTO registrar) {
-        return new Button(constants.details(), new ClickHandler() {
-
-            @Override
-            public void onClick(ClickEvent event) {
-                superPanel.showRegistrarDetails(registrar);
-            }
-        });
-    }
-
-    private int registrarGridColumns() {
-        if (user.isSuperAdmin()) {
-            return 3;
-        } else if (user.isLoggedUser()) {
-            return 2;
-        } else {
-            return 2;
-        }
-    }
-
-    private Button addRegistrarButton() {
-        return new Button(constants.add(), new ClickHandler() {
-
-            @Override
-            public void onClick(ClickEvent event) {
-                AddRegistrarDialogBox dialogBox = new AddRegistrarDialogBox(InstitutionListPanel.this);
-                dialogBox.show();
-            }
-        });
-    }
-
     private Widget archiversHeading() {
         Label label = new Label(constants.archiverList());
         label.addStyleName(css.listHeading());
         return label;
     }
 
-    private interface GridHelper<T extends ArchiverDTO> {
-
-        public Button createDetailsButton(T item);
-
-        public Button createEditButton(T item);
-
-        public Button createDeleteButton(T item);
-
-        public void update(List<T> items, AsyncCallback<Void> callBack);
-    }
-
-    private class ArchiversGridHelper implements GridHelper<ArchiverDTO> {
-
-        public Button createDetailsButton(ArchiverDTO item) {
-            return archiverDetailsButton(item);
-        }
-
-        public Button createEditButton(ArchiverDTO item) {
-            return archiverEditButton(item);
-        }
-
-        public Button createDeleteButton(ArchiverDTO item) {
-            return archiverDeleteButton(item);
-        }
-
-        public void update(List<ArchiverDTO> items, AsyncCallback<Void> callBack) {
-            institutionsService.updateArchivers(items, callBack);
-        }
-    }
-
-    private AbsolutePanel buildRegistrarGrid(List<RegistrarDTO> registrars) {
+    private AbsolutePanel buildRegistrarsGrid(List<RegistrarDTO> registrars) {
         final AbsolutePanel panel = new AbsolutePanel();
         CellTable<RegistrarDTO> table = new CellTable<>();
         table.setPageSize(registrars.size());
@@ -264,8 +161,8 @@ public class InstitutionListPanel extends VerticalPanel {
         //name
         TextColumn<RegistrarDTO> nameColumn = new TextColumn<RegistrarDTO>() {
             @Override
-            public String getValue(RegistrarDTO object) {
-                return object.getName();
+            public String getValue(RegistrarDTO registrar) {
+                return registrar.getName();
             }
         };
         table.addColumn(nameColumn, constants.title());
@@ -284,8 +181,8 @@ public class InstitutionListPanel extends VerticalPanel {
         actionCells.add(new RegistrarActionHasCell(constants.details(), new ActionCell.Delegate<RegistrarDTO>() {
 
             @Override
-            public void execute(RegistrarDTO object) {
-                superPanel.showRegistrarDetails(object);
+            public void execute(RegistrarDTO registrar) {
+                showRegistrarDetails(registrar);
             }
         }));
         //delete button
@@ -293,8 +190,8 @@ public class InstitutionListPanel extends VerticalPanel {
             actionCells.add(new RegistrarActionHasCell(constants.delete(), new ActionCell.Delegate<RegistrarDTO>() {
 
                 @Override
-                public void execute(RegistrarDTO object) {
-                    deleteRegistrar(object);
+                public void execute(RegistrarDTO registrar) {
+                    deleteRegistrar(registrar);
                 }
             }));
         }
@@ -302,8 +199,8 @@ public class InstitutionListPanel extends VerticalPanel {
         Column<RegistrarDTO, RegistrarDTO> actionsColumn = new Column<RegistrarDTO, RegistrarDTO>(actionsCell) {
 
             @Override
-            public RegistrarDTO getValue(RegistrarDTO object) {
-                return object;
+            public RegistrarDTO getValue(RegistrarDTO registrar) {
+                return registrar;
             }
         };
         table.addColumn(actionsColumn);
@@ -332,12 +229,13 @@ public class InstitutionListPanel extends VerticalPanel {
         table.addColumnSortHandler(columnSortHandler);
         // By default sorted by name
         table.getColumnSortList().push(nameColumn);
+        ColumnSortEvent.fire(table, table.getColumnSortList());
 
         panel.add(table);
         return panel;
     }
 
-    private AbsolutePanel buildArchiverGrid(ArrayList<ArchiverDTO> archivers) {
+    private AbsolutePanel buildArchiversGrid(ArrayList<ArchiverDTO> archivers) {
         final AbsolutePanel panel = new AbsolutePanel();
         CellTable<ArchiverDTO> table = new CellTable<>();
         table.setPageSize(archivers.size());
@@ -346,8 +244,8 @@ public class InstitutionListPanel extends VerticalPanel {
         //name
         TextColumn<ArchiverDTO> nameColumn = new TextColumn<ArchiverDTO>() {
             @Override
-            public String getValue(ArchiverDTO object) {
-                return object.getName();
+            public String getValue(ArchiverDTO archiver) {
+                return archiver.getName();
             }
         };
         table.addColumn(nameColumn, constants.title());
@@ -358,8 +256,8 @@ public class InstitutionListPanel extends VerticalPanel {
         actionCells.add(new ArchiverActionHasCell(constants.details(), new ActionCell.Delegate<ArchiverDTO>() {
 
             @Override
-            public void execute(ArchiverDTO object) {
-                showArchiverDetails(object);
+            public void execute(ArchiverDTO archiver) {
+                showArchiverDetails(archiver);
             }
         }));
         if (user.isSuperAdmin()) {
@@ -383,8 +281,8 @@ public class InstitutionListPanel extends VerticalPanel {
         Column<ArchiverDTO, ArchiverDTO> actionsColumn = new Column<ArchiverDTO, ArchiverDTO>(actionsCell) {
 
             @Override
-            public ArchiverDTO getValue(ArchiverDTO object) {
-                return object;
+            public ArchiverDTO getValue(ArchiverDTO archiver) {
+                return archiver;
             }
         };
         table.addColumn(actionsColumn);
@@ -404,108 +302,30 @@ public class InstitutionListPanel extends VerticalPanel {
             }
         });
         table.addColumnSortHandler(columnSortHandler);
+
         // By default sorted by name
         table.getColumnSortList().push(nameColumn);
+        ColumnSortEvent.fire(table, table.getColumnSortList());
 
         panel.add(table);
         return panel;
     }
 
 
-    private <T extends ArchiverDTO> AbsolutePanel getGrid(List<T> list, final GridHelper<T> gridHelper) {
-        final AbsolutePanel panel = new AbsolutePanel();
+    /* archiver CRUD methods */
 
-
-    /*    final FlexTable table = new FlexTable();
-        for (int row = 0; row < list.size(); row++) {
-            T archiver = list.get(row);
-            CustomHTML<T> handle = new CustomHTML<T>(archiver.getName(), archiver);
-            table.setWidget(row, 0, handle);
-            Button detailsButton = gridHelper.createDetailsButton(archiver);
-            table.setWidget(row, 1, detailsButton);
-            if (user.isSuperAdmin()) {
-                int pos = 2;
-                Button edit = gridHelper.createEditButton(archiver);
-                if (edit != null) {
-                    table.setWidget(row, pos, edit);
-                    pos++;
-                }
-                Button delete = gridHelper.createDeleteButton(archiver);
-                if (delete != null) {
-                    table.setWidget(row, pos, delete);
-                    pos++;
-                }
-            }
-        }
-        panel.add(table);
-        if (user.isSuperAdmin()) {
-            final Button saveButton = new Button(constants.save());
-            saveButton.addClickHandler(new ClickHandler() {
-                public void onClick(ClickEvent arg0) {
-                    List<T> items = new ArrayList<T>();
-                    for (int row = 0; row < table.getRowCount(); row++) {
-                        CustomHTML<T> widget = (CustomHTML<T>) table.getWidget(row, 0);
-                        T archiver = widget.getObject();
-                        items.add(archiver);
-                    }
-
-                    gridHelper.update(items, new AsyncCallback<Void>() {
-
-                        public void onFailure(Throwable caught) {
-                            logger.severe(caught.getMessage());
-                        }
-
-                        public void onSuccess(Void arg0) {
-                            // nothing
-                        }
-
-                    });
-                }
-            });
-            panel.add(saveButton);
-            //FlexTableRowDropController flexTableRowDropController = new FlexTableRowDropController(table);
-            //tableRowDragController.registerDropController(flexTableRowDropController);
-        }
-        */
-
-        return panel;
+    private void addArchiver() {
+        new AddArchiverDialogBox(InstitutionListPanel.this).show();
     }
 
-    private AbsolutePanel buildArchiversGrid() {
-        //return getGrid(archivers, new ArchiversGridHelper());
-        return buildArchiverGrid(archivers);
-    }
-
-    private AbsolutePanel buildRegistrarsGrid() {
-        return buildRegistrarGrid(registrars);
-    }
-
-    @Deprecated
-    private Button archiverEditButton(final ArchiverDTO archiver) {
-        return new Button(constants.edit(), new ClickHandler() {
-
-            @Override
-            public void onClick(ClickEvent event) {
-                editArchiver(archiver);
-            }
-        });
+    private void showArchiverDetails(ArchiverDTO archiver) {
+        ArchiverDetailsDialogBox dialogBox = new ArchiverDetailsDialogBox(archiver);
+        dialogBox.show();
+        dialogBox.center();
     }
 
     private void editArchiver(ArchiverDTO archiver) {
         new EditArchiverDialogBox(InstitutionListPanel.this, archiver).show();
-    }
-
-    @Deprecated
-    private Button archiverDeleteButton(final ArchiverDTO archiver) {
-        Button result = new Button(constants.delete());
-        result.addClickHandler(new ClickHandler() {
-
-            @Override
-            public void onClick(ClickEvent event) {
-                deleteArchiver(archiver);
-            }
-        });
-        return result;
     }
 
     private void deleteArchiver(final ArchiverDTO archiver) {
@@ -526,46 +346,33 @@ public class InstitutionListPanel extends VerticalPanel {
         }
     }
 
-    @Deprecated
-    private Button archiverDetailsButton(final ArchiverDTO archiver) {
-        Button result = new Button(constants.details());
-        result.addClickHandler(new ClickHandler() {
+    /*registrar CRUD*/
 
-            @Override
-            public void onClick(ClickEvent event) {
-                showArchiverDetails(archiver);
-            }
-        });
-        return result;
-    }
-
-    private void showArchiverDetails(ArchiverDTO archiver) {
-        ArchiverDetailsDialogBox dialogBox = new ArchiverDetailsDialogBox(archiver);
+    private void addRegistrar() {
+        AddRegistrarDialogBox dialogBox = new AddRegistrarDialogBox(InstitutionListPanel.this);
         dialogBox.show();
-        dialogBox.center();
     }
 
-    private int archiverGridColumns() {
-        if (user.isSuperAdmin()) {
-            return 4;
-        } else if (user.isLoggedUser()) {
-            return 2;
-        } else {
-            return 2;
+    private void showRegistrarDetails(RegistrarDTO registrar) {
+        superPanel.showRegistrarDetails(registrar);
+    }
+
+    private void deleteRegistrar(final RegistrarDTO registrar) {
+        if (Window.confirm(messages.confirmDeleteRegistrar(registrar.getName()))) {
+            institutionsService.deleteRegistrar(registrar, new AsyncCallback<Void>() {
+
+                @Override
+                public void onSuccess(Void result) {
+                    registrars.remove(registrar);
+                    reload();
+                }
+
+                @Override
+                public void onFailure(Throwable caught) {
+                    Window.alert(messages.registrarCannotBeDeleted(registrar.getName()) + ": " + caught.getMessage());
+                }
+            });
         }
     }
 
-    private Button addArchiverButton() {
-        return new Button(constants.add(), new ClickHandler() {
-
-            @Override
-            public void onClick(ClickEvent event) {
-                new AddArchiverDialogBox(InstitutionListPanel.this).show();
-            }
-        });
-    }
-
-    public UserDTO getUser() {
-        return user;
-    }
 }
