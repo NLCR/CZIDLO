a3fa8cb63812d240fb371996704ef3913f870809
Martin Řehánek <Martin.Rehanek@gmail.com>
2017-12-07 17:20:34 +0100

    drobna oprava komentaru

--- processDataServer/src/main/java/cz/nkp/urnnbn/processdataserver/TransformationsResource.java
+++ processDataServer/src/main/java/cz/nkp/urnnbn/processdataserver/TransformationsResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Martin Řehánek
+ * Copyright (C) 2017 Martin Řehánek
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
