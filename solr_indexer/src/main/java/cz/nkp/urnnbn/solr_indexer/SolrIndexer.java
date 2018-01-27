ca19f17b69e540ce742aac6fd7a736fb6d5f7bbb
Martin Řehánek <Martin.Rehanek@gmail.com>
2018-01-27 23:58:31 +0100

    solr indexer - oprava ve formatovani doby prubehu

--- solr_indexer/src/main/java/cz/nkp/urnnbn/solr_indexer/SolrIndexer.java
+++ solr_indexer/src/main/java/cz/nkp/urnnbn/solr_indexer/SolrIndexer.java
@@ -195,7 +195,7 @@ public class SolrIndexer {
 
     private String formatTime(long millis) {
         long hours = millis / (60 * 60 * 1000);
-        long minutes = millis / (60 * 1000);
+        long minutes = millis / (60 * 1000) - hours * 60;
         long seconds = (millis / 1000) % 60;
         return String.format("%d:%02d:%02d", hours, minutes, seconds);
     }
