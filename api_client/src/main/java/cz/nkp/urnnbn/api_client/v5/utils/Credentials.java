2048203bdd065b3f147e97e78a987dc20abe1831
Martin Řehánek <martin.rehanek@gmail.com>
2020-04-23 14:17:18 +0200

    Change implementation of base64 encoding, which differs between java 7 to java 8.

--- api_client/src/main/java/cz/nkp/urnnbn/api_client/v5/utils/Credentials.java
+++ api_client/src/main/java/cz/nkp/urnnbn/api_client/v5/utils/Credentials.java
@@ -1,5 +1,7 @@
 package cz.nkp.urnnbn.api_client.v5.utils;
 
+import java.nio.charset.StandardCharsets;
+
 public class Credentials {
 
     private final String login;
@@ -14,10 +16,18 @@ public class Credentials {
     public String getBasicAccessAuthorizationHeader() {
         if (basicAccessAuthToken == null) {
             String concatenated = login + ":" + password;
-            String base64Encoded = new sun.misc.BASE64Encoder().encode(concatenated.getBytes());
+            String base64Encoded = base64Encode(concatenated);
             basicAccessAuthToken = "Basic " + base64Encoded;
         }
         return basicAccessAuthToken;
+    }
+
+    private String base64Encode(String concatenated) {
+        byte[] bytes = concatenated.getBytes();
+        //funguje do javy <=7, v novejsi jave nelze zkompilovat
+        //return new sun.misc.BASE64Encoder().encode(bytes);
+        //funguje od javy 8+
+        return new String(java.util.Base64.getEncoder().encode(bytes), StandardCharsets.UTF_8);
     }
 
     public String getLogin() {
