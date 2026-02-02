3cde67993445c1eab89235baf91fb6297772f8c2
Martin Řehánek <martin.rehanek@trinera.cz>
2026-02-02 01:46:27 +0100

    cleanup

--- core/src/main/java/cz/nkp/urnnbn/core/AdminLogger.java
+++ core/src/main/java/cz/nkp/urnnbn/core/AdminLogger.java
@@ -31,24 +31,27 @@ import org.apache.log4j.PatternLayout;
  */
 public class AdminLogger {
 
-    private static Logger logger;
-    private static File logFile;
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(AdminLogger.class.getName());
+
+    private static Logger adminLogger;
+    private static File adminLogFile;
 
     public static void initializeLogger(String loggerName, File loggerFile) throws IOException {
-        logFile = loggerFile;
-        AdminLogger.logger = Logger.getLogger(loggerName);
+        logger.info("Initializing admin logger '" + loggerName + "' with log file: " + loggerFile.getAbsolutePath());
+        adminLogFile = loggerFile;
+        AdminLogger.adminLogger = Logger.getLogger(loggerName);
         // Layout layout = new PatternLayout("%-5p [%d{dd. MM. yyyy HH:mm:ss}] %c: %m%n");
         Layout layout = new PatternLayout("[%d{dd.MM.yyyy, HH:mm:ss}] %c: %m%n");
         Appender appender = new FileAppender(layout, loggerFile.getAbsolutePath(), true);
-        logger.removeAllAppenders();
-        logger.addAppender(appender);
+        adminLogger.removeAllAppenders();
+        adminLogger.addAppender(appender);
     }
 
     public static Logger getLogger() {
-        return logger;
+        return adminLogger;
     }
 
     public static File getLogFile() {
-        return logFile;
+        return adminLogFile;
     }
 }
